b7cba1e30c4e796de6bc9f58ef9fcef8d40554fb
Chris <cmbrauch00@gmail.com>
2020-06-04 15:08:11 -0400

    add more parameters to scripts.js
    <Co-authored-by: Taylor Phillips <TaylorPhillips133@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-    $("form#insurance").submit(function(event) {
-      const age = parseInt($("input#age").val());
-      const gender = $("select#gender").val();
-  
-      const quote = (100 - age) * 3;
-  
+  $("form#insurance").submit(function(event) {
+    const age = parseInt($("input#age").val());
+    const gender = $("select#gender").val();
+
+    if (age) {
+      let quote = (100 - age) * 3;
+      if (gender === 'male' || age < 26) {
+        quote += 50;
+      if (!under18) {
+            // do something only adults can do
+          }
+      }
+
       $("#rate").text(quote);
       $("#quote").show();
-  
-      event.preventDefault();
-    });
+    } else {
+      alert('Please enter your age.');
+    }
+
+    event.preventDefault();
   });
+});
